b6080916a6e61ace4f92bb18ae67452e3b85706c
sadafparveen22 <sadafansari2212@gmail.com>
2026-01-25 02:45:07 +0530

    Change address_space to address_spaces in VNET resource

--- modules/azurerm_virtual_network/main.tf
+++ modules/azurerm_virtual_network/main.tf
@@ -3,7 +3,8 @@ resource "azurerm_virtual_network" "VNET" {
   name                = each.value.name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
-address_space = each.value.address_space
+address_space = each.value.address_spaces
+
 
 
   dynamic "subnet" {
@@ -16,3 +17,4 @@ address_space = each.value.address_space
 
   tags = var.tags
 }
+
