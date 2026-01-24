c9b64e6e539a40ebaab488ce65e09c7d4b2b5b5a
sadafparveen22 <sadafansari2212@gmail.com>
2026-01-25 02:40:59 +0530

    add

--- modules/azurerm_virtual_network/main.tf
+++ modules/azurerm_virtual_network/main.tf
@@ -3,7 +3,8 @@ resource "azurerm_virtual_network" "VNET" {
   name                = each.value.name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
-  address_space       = each.value.address_space
+address_space = each.value.address_space
+
 
   dynamic "subnet" {
     for_each = each.value.subnets
